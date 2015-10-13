0a77bdb919eb015adc19bb2ad6471b12fece23e4
andre <freitaga@in.tum.de>
2015-10-13 18:07:35 +0200

    added copyright remark

--- yarsnap.py
+++ yarsnap.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
-"""
-Yet Another Rsync SNAPshot utility.
-"""
+# -*- coding: UTF-8 -*-
+#
+# Yet Another Rsync SNAPshot utility (yarsnap).
+# Copyright 2015 André Freitag <andre@freitag.solutions>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import datetime

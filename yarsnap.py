107b03efc379dcd5c33edeea100c70f780dd62b3
andre <freitaga@in.tum.de>
2015-10-18 14:03:17 +0200

    adjusted log formatting

--- yarsnap.py
+++ yarsnap.py
@@ -358,9 +358,9 @@ if __name__ == "__main__":
         2: logging.DEBUG
     }[args.verbosity]
 
-    log_format = "%(levelname)s - %(asctime)s:{}\t%(message)s".format(os.linesep)
+    log_format = "%(levelname)s: %(message)s"
     if args.action == "__service":
-        log_format = "%(levelname)s (__service) - %(asctime)s:{}\t%(message)s".format(os.linesep)
+        log_format = "__service:%(levelname)s: %(message)s"
 
     logging.basicConfig(level=log_level, format=log_format)
 
